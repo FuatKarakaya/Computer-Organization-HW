7ae52d7330ff941dff04ff3ba9b5d3f307113623
Mehmet Fuat Karakaya <68186893+FuatKarakaya@users.noreply.github.com>
2025-04-18 23:26:57 +0300

    Update ArithmeticLogicUnitSystem.v

--- ArithmeticLogicUnitSystem.v
+++ ArithmeticLogicUnitSystem.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.04.2025 15:49:13
-// Design Name: 
-// Module Name: ArithmeticLogicUnitSystem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module ArithmeticLogicUnitSystem(
 input wire Clock,
